74e2774357175c54de1ad6b833f2548f66495423
Youness Zioual <crness2@gmail.com>
2025-10-21 12:25:03 +0200

    mod: updated Router class

--- fixme/src/main/java/com/fortytwo/fixme/router/Router.java
+++ fixme/src/main/java/com/fortytwo/fixme/router/Router.java
@@ -1,18 +1,22 @@
 package com.fortytwo.fixme.router;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.UUID;
+import java.sql.Time;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Router {
     private static final int BUFFER_SIZE = 1024;
     private static Router instance = null;
     public final int BROKER_PORT = 5000;
     public final int MARKET_PORT = 5001;
+    private long id = 111111;
+    private MessageType messageType = MessageType.ACK;
 
     private Router() {
     }
@@ -25,7 +29,7 @@ public class Router {
     }
 
     private String getUniqueId() {
-        return UUID.randomUUID().toString();
+        return Long.toString(id++);
     }
 
     private String getChecksum(int len) {
@@ -33,36 +37,67 @@ public class Router {
     }
 
     private void handleBroker(Socket socket) throws IOException {
-        // the first key-value is the msgType(35), ID refers to first message sent by the Router communicating the ID
-        OutputStream out = socket.getOutputStream();
-        String initMessage = "35=ID|56=" + getUniqueId() + "|10=";
-        String firstMessage = initMessage + getChecksum(initMessage.length());
-        out.write(firstMessage.getBytes());
     }
 
-    public void listen(int port) {
+    private void handleMarket(Socket socket) {
+    }
+
+    private void sendAck(Socket socket) throws IOException {
+        System.out.println("Sending Client its assigned ID ");
+        OutputStream out = socket.getOutputStream();
+        String ack = "35=ID|56=" + getUniqueId();
+        out.write(ack.getBytes());
+        out.flush();
+        this.messageType = MessageType.ORDINARY;
+    }
+
+    private void listen(int port){
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.println("Listening on port " + socket.getLocalPort());
-                    if (socket.isConnected()) {
-                        System.out.println("Accepted connection from a Broker " + socket.getInetAddress());
+                Socket socket = serverSocket.accept();
+                System.out.println("Listening on port " + socket.getLocalPort());
+                if (socket.isConnected()) {
+                    System.out.println("Accepted connection from Broker");
+                    if (messageType == MessageType.ACK) {
+                        sendAck(socket);
+                    } else if (messageType == MessageType.ORDINARY) {
                         if (socket.getLocalPort() == BROKER_PORT) {
-                            System.out.println("Handling a Broker " + socket.getInetAddress());
                             handleBroker(socket);
                         } else if (socket.getLocalPort() == MARKET_PORT) {
                             handleMarket(socket);
                         } else {
-                            throw new IllegalStateException("Invalid port " + socket.getLocalPort());
+                            throw new IllegalArgumentException("Invalid port number");
                         }
                     }
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error accepting socket");
         }
     }
 
-    private void handleMarket(Socket socket) {
+    public void brokerService() {
+        listen(BROKER_PORT);
+    }
+
+    public void marketService() {
+        listen(MARKET_PORT);
+    }
+
+    /**
+     * Router is listening onto port 5000 & 5001 in parallel via thread pools
+     */
+    public void activate() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        try {
+            executor.execute(this::brokerService);
+            executor.execute(this::marketService);
+            if (executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Executor was interrupted: " + e.getMessage());
+        }
     }
 }
