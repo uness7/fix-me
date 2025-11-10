df198481ac5a9a014e98116dc454cce890e67b6f
Youness Zioual <crness2@gmail.com>
2025-11-10 13:40:06 +0100

    mod: testing if the parsing works as expected

--- fixme/src/main/java/com/fortytwo/fixme/App.java
+++ fixme/src/main/java/com/fortytwo/fixme/App.java
@@ -2,16 +2,29 @@ package com.fortytwo.fixme;
 
 import com.fortytwo.fixme.common.Utils;
 import com.fortytwo.fixme.market.Market;
+import com.fortytwo.fixme.common.Instrument;
+import java.util.ListIterator;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 class Main {
     public static void main(String[] args) {
-        Market mr = new Market("Blue", Utils.getInstruments("~/notes.txt"));
+		LinkedList<Instrument> instruments = 
+			Utils.getInstruments("config.txt");
+		ListIterator<Instrument> it = instruments.listIterator();
+
+		while (it.hasNext()) {
+			Instrument ins = it.next();
+			System.out.println(ins.toString());
+		}
+
+		/*
         try {
             mr.activate();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+		*/
     }
 }
